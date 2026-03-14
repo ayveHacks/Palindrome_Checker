cd0c8722342ce8d0903abdb43857a1a24e20a62f
ayveHacks <ayushverma5490@gmail.com>
2026-03-14 18:03:19 +0530

    UC2 added

---
+++ UC2.java
@@ -0,0 +1,16 @@
+public class UC2 {
+    public static void main(String[] args) {
+        String text = "madam";
+        String reversed = "";
+
+        for (int i = text.length() - 1; i >= 0; i--) {
+            reversed = reversed + text.charAt(i);
+        }
+
+        if (text.equals(reversed)) {
+            System.out.println("The string \"" + text + "\" is a palindrome.");
+        } else {
+            System.out.println("The string \"" + text + "\" is not a palindrome.");
+        }
+    }
+}
